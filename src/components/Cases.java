39bf863e546bcfb1ff306f0cc61e5af581b4d398
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-15 21:46:57 +0100

    methods for validation moved to 'Validators' class

--- src/components/Cases.java
+++ src/components/Cases.java
@@ -30,32 +30,8 @@ public class Cases {
         return FileIO.appendToFile(toAdd, Config.FILE.path);
     };
 
-    // returns -1 invalid input
-    private static int validateInput(String[] args) {
-        long linesCount = FileIO.countLines(Config.FILE.path);
-
-        if (linesCount == 0) {
-            print("file is empty");
-            return -1;
-        }
-
-        if (!validInteger(args)) {
-            print("Unable to check: index is not a number");
-            return -1;
-        }
-
-        int todoToHandleIndex = Integer.parseInt(args[1]) - 1;
-
-        if (todoToHandleIndex < 0 || todoToHandleIndex >= linesCount) {
-            print("Unable to reach: index is out of bounds");
-            return -1;
-        }
-
-        return todoToHandleIndex;
-    }
-
     public static void checkTodo(String[] args) {
-        int todoToHandleIndex = validateInput(args);
+        int todoToHandleIndex = Validators.validateInput(args);
 
         // provided input was not valid
         if (todoToHandleIndex == -1) {
@@ -79,7 +55,7 @@ public class Cases {
     }
 
     public static void removeTodo(String[] args) {
-        int todoToHandleIndex = validateInput(args);
+        int todoToHandleIndex = Validators.validateInput(args);
 
         // provided input was not valid
         if (todoToHandleIndex == -1) {
@@ -100,16 +76,6 @@ public class Cases {
         FileIO.deleteFile(Config.FILE.path);
         InitProcedure.start();
         return lines;
-    }
-
-    private static boolean validInteger(String[] args) {
-        try {
-            Integer.parseInt(args[1]);
-            return true;
-        } catch (NumberFormatException e) {
-            print("Unable to remove: index is not a number");
-            return false;
-        }
     }
 
     public static void showTodos() {
---
+++ src/components/Validators.java
@@ -0,0 +1,43 @@
+package components;
+
+import common.FileIO;
+import enums.Config;
+
+import static common.OutToConsole.print;
+
+public class Validators {
+
+    // returns -1 invalid input
+    protected static int validateInput(String[] args) {
+        long linesCount = FileIO.countLines(Config.FILE.path);
+
+        if (linesCount == 0) {
+            print("file is empty");
+            return -1;
+        }
+
+        if (!validInteger(args)) {
+            print("Unable to check: index is not a number");
+            return -1;
+        }
+
+        int todoToHandleIndex = Integer.parseInt(args[1]) - 1;
+
+        if (todoToHandleIndex < 0 || todoToHandleIndex >= linesCount) {
+            print("Unable to reach: index is out of bounds");
+            return -1;
+        }
+
+        return todoToHandleIndex;
+    }
+
+    protected static boolean validInteger(String[] args) {
+        try {
+            Integer.parseInt(args[1]);
+            return true;
+        } catch (NumberFormatException e) {
+            print("Unable to remove: index is not a number");
+            return false;
+        }
+    }
+}
