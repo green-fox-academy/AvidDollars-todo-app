f2f550a4cb379cfca19d736ce1d604afeb5c47d4
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-15 10:28:38 +0100

    add File with static methods for printing out to console

---
+++ src/consoleout/OutToConsole.java
@@ -0,0 +1,14 @@
+package consoleout;
+
+// import it with static keyword:
+// eg.: import static consoleout.OutToConsole.*;
+
+public class OutToConsole {
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void printf(String format, Object... objects) {
+        System.out.printf(format, objects);
+    }
+}
