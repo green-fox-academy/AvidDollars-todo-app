515c12e26a374a9812a4aa15ad662cade03420c9
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-16 18:19:21 +0100

    Configuration class reimplemented, so that the app can handle multiple users

--- src/components/Configuration.java
+++ src/components/Configuration.java
@@ -1,14 +1,39 @@
 package components;
 
-// contains names for files and folders for the app
-public enum Configuration {
-    FOLDER("src/todo-files"),
-    FILE("src/todo-files/todo-tasks");
+import common.FileIO;
+import static common.OutToConsole.*;
 
-    public final String path;
+public class Configuration {
+    private static final String DEFAULT_USER = "default";
+    private static final String TODO_FOLDER = "src/.todo-files";
+    private static final String ACTUAL_USER_FILE = ".actual-user";
 
-    private Configuration(String path) {
-        this.path = path;
+    private static void createFileAndSetUser(String userName) {
+        FileIO.createFileIfAbsent(ACTUAL_USER_FILE);
+        FileIO.writeToFile(userName, ACTUAL_USER_FILE);
     }
 
+    private static String getActualUser() {
+        return FileIO.readFirstLine(ACTUAL_USER_FILE);
+    }
+
+    public static String getTodoFolder() {
+        return TODO_FOLDER;
+    }
+
+    public static void setCurrentUser(String userName) {
+        //FileIO.deleteFile(ACTUAL_USER_FILE);
+        createFileAndSetUser(userName);
+    }
+
+    public static String getFileForCurrentUser() {
+        String actualUser = getActualUser();
+        return String.format("%s/%s", TODO_FOLDER, actualUser);
+    }
+
+    public static void initializeDefaultUser() {
+        if (!FileIO.fileExists(ACTUAL_USER_FILE)) {
+            createFileAndSetUser(DEFAULT_USER);
+        }
+    }
 }
