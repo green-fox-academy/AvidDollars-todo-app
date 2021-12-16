229cc951e7d47548dc74fb1387a1c2ad57425ff4
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-12-16 18:21:17 +0100

    modification of the main class

--- src/App.java
+++ src/App.java
@@ -3,33 +3,14 @@ import components.*;
 import components.Configuration;
 
 public class App {
-    private static boolean hasTodoFolderAndFile = false;
-
     public static void main(String[] args) {
-        createFileAndFolderIfNeeded();
-
-        // ↓↓↓ dummy data for simulating CLI input, to be removed
-        /*args = new String[]{
-                "-r", "2"
-        };*/
-
-        // end of dummy data
-
+        createFolderAndFileIfNeeded();
         ArgumentHandler.handle(args);
-
-    }
-
-    // 1st run of the app → creation of folder and file for the app
-    private static void createFileAndFolderIfNeeded() {
-        if (!hasTodoFolderAndFile) {
-            createFolderAndFileIfNeeded();
-            hasTodoFolderAndFile = true;
-        }
     }
 
     private static void createFolderAndFileIfNeeded() {
-        FileIO.createFolderIfAbsent(Configuration.FOLDER.path);
-        FileIO.createFileIfAbsent(Configuration.FILE.path);
+        Configuration.initializeDefaultUser();
+        FileIO.createFolderIfAbsent(Configuration.getTodoFolder());
+        FileIO.createFileIfAbsent(Configuration.getFileForCurrentUser());
     }
-
 }
